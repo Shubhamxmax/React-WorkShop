e4f3c47cff5af6c7c3ff52166208f34e94c597dd
Shubhamxmax <shubh101bhagat@gmail.com>
2026-03-08 23:25:26 +0530

    Context API

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -39,7 +39,7 @@ useEffect(() => {
 
   return (
    <Themeprovider value={{thememode,lightheme,darktheme}}>
-  <div className="flex flex-wrap min-h-screen items-center bg-white dark:bg-gray-900">
+  <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
       <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
         <ThemeBtn/>
