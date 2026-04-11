2c0df7107f55567587f5c082f3945dfdfdd7e927
Shubhamxmax <shubh101bhagat@gmail.com>
2026-04-11 16:08:54 +0530

    Redux Completed

--- 11Redux/react-redux-toolkit/src/App.jsx
+++ 11Redux/react-redux-toolkit/src/App.jsx
@@ -1,15 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { reset, increment, decrement, incrementByAmount } from "./features/counter/counterSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const count = useSelector((state) => state.counter.value);//
+  const dispatch = useDispatch();
+
+  function handleIncrementClick() {
+    dispatch(increment());
+  }
+
+  function handleDecrementClick() {
+    dispatch(decrement());
+  }
+
+  function handleResetClick() {
+    dispatch(reset());
+  }
+  function handleIncAmountClick() {
+    dispatch(incrementByAmount(amount))
+  }
 
   return (
-  <div>Hii</div>
-  )
+    <div className="container">
+      <button onClick={handleIncrementClick}> + </button>
+      <p>Count:{count}</p>
+      <button onClick={handleDecrementClick}> - </button>
+      <br />
+      <br />
+      <button onClick={handleResetClick}> Reset </button>
+      <br />
+      <br />
+      <input
+        type="number"
+        value={amount}
+        placeholder="Enter Amount"
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={handleIncAmountClick}> Increment by amount </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- 11Redux/react-redux-toolkit/src/redux/store.js
+++ 11Redux/react-redux-toolkit/src/redux/store.js
@@ -1,11 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 
 
-const store = configureStore({
+
+const Store = configureStore({
       reducer:{
         counter:counterReducer
       }
 })
 
-export default store
+export default Store
+
+
+//steps:
+
+// Create Store 
+// Wrap app componenet under Provider
+// Create createSlice
+// Register reducer in Store
